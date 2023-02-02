d9b3acef90f0e2fb6ec994a8e8b2f8e5aeb42bcc
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-02-03 00:22:26 +0530

    changed font-size of title on home screen.

--- VideoList.js
+++ VideoList.js
@@ -38,14 +38,16 @@ const styles = StyleSheet.create({
     "thumbnail": {
         "width": "100vw",
         "height": "100vw",
-        "marginTop": 5
+        "marginTop": 5,
+        "backgroundColor": "#E3E0A8"
     },
     "video_title": {
-        "color": "hsl(274,100%,60%)",
+        "color": "#F8271A",
         "fontSize": 15,
         "marginTop": 5,
         "fontWeight": "400",
         "marginHorizontal": 10,
-        "paddingHorizontal": 2
+        "paddingHorizontal": 2,
+        "fontFamily": "monospace"
     }
 });
